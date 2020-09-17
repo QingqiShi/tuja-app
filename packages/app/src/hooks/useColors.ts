4f0da98448c8aecd883fc038eb81b03efdc84f3d
Qingqi Shi <qingqishi@live.com>
2020-09-17 20:02:00 +0100

    fix(eslint): lint warnings

--- packages/app/src/hooks/useColors.ts
+++ packages/app/src/hooks/useColors.ts
@@ -9,34 +9,27 @@ function useColors() {
   const styledTheme = useTheme();
   const { portfolioPerformance } = usePortfolioPerformance();
 
-  const domain = portfolioPerformance
-    ? Object.keys(portfolioPerformance.holdings)
-        .map((ticker) => ticker)
-        .sort((a, b) => {
-          return (
-            portfolioPerformance.holdings[b].value -
-            portfolioPerformance.holdings[a].value
-          );
-        })
-        .concat('Cash')
-    : [];
-
-  const getColor = useMemo(
-    () =>
-      scaleOrdinal({
-        domain,
-        range: [
-          ...theme.colors.ordinals({ theme: styledTheme }),
-          ...theme.colors
-            .ordinals({ theme: styledTheme })
-            .map((color) => transparentize(0.3, color)),
-          ...theme.colors
-            .ordinals({ theme: styledTheme })
-            .map((color) => transparentize(0.6, color)),
-        ],
-      }),
-    [domain, styledTheme]
-  );
+  const getColor = useMemo(() => {
+    const holdings = portfolioPerformance?.holdings;
+    const domain = holdings
+      ? Object.keys(holdings ?? {})
+          .map((ticker) => ticker)
+          .sort((a, b) => holdings[b].value - holdings[a].value)
+          .concat('Cash')
+      : [];
+    return scaleOrdinal({
+      domain,
+      range: [
+        ...theme.colors.ordinals({ theme: styledTheme }),
+        ...theme.colors
+          .ordinals({ theme: styledTheme })
+          .map((color) => transparentize(0.3, color)),
+        ...theme.colors
+          .ordinals({ theme: styledTheme })
+          .map((color) => transparentize(0.6, color)),
+      ],
+    });
+  }, [portfolioPerformance?.holdings, styledTheme]);
   return getColor;
 }
 
