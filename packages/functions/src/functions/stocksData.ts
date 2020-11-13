d0a22ab32566c3621e82d6959e35e1733e89661f
Qingqi Shi <qingqishi@live.com>
2020-11-13 23:39:33 +0000

    feat(functions): increase function memory for faster instances

--- packages/functions/src/functions/stocksData.ts
+++ packages/functions/src/functions/stocksData.ts
@@ -11,55 +11,59 @@ Searching
 
 const searchStocksCache = new NodeCache();
 
-export const searchStocks = functions.https.onCall(async (data) => {
-  const { query } = data;
-  if (typeof query !== 'string') {
-    throw new functions.https.HttpsError(
-      'invalid-argument',
-      'You need to provide a search query'
+export const searchStocks = functions
+  .runWith({ memory: '1GB' })
+  .https.onCall(async (data) => {
+    const { query } = data;
+    if (typeof query !== 'string') {
+      throw new functions.https.HttpsError(
+        'invalid-argument',
+        'You need to provide a search query'
+      );
+    }
+
+    functions.logger.log('query', { query });
+
+    const cache = searchStocksCache.get(query);
+    if (cache) {
+      functions.logger.log('cache hit', { key: query });
+      return cache;
+    }
+
+    functions.logger.log('cache miss', { key: query });
+    const response = await fetch(
+      `https://eodhistoricaldata.com/api/search/${query}?api_token=${getToken()}`
     );
-  }
+    const result = await response.json();
 
-  functions.logger.log('query', { query });
-
-  const cache = searchStocksCache.get(query);
-  if (cache) {
-    functions.logger.log('cache hit', { key: query });
-    return cache;
-  }
-
-  functions.logger.log('cache miss', { key: query });
-  const response = await fetch(
-    `https://eodhistoricaldata.com/api/search/${query}?api_token=${getToken()}`
-  );
-  const result = await response.json();
-
-  searchStocksCache.set(query, result);
-  return result;
-});
+    searchStocksCache.set(query, result);
+    return result;
+  });
 
 /* =====
 Stock live price
 */
 
-export const stockLivePrice = functions.https.onCall(async (data) => {
-  const { ticker } = data;
-  if (typeof ticker !== 'string') {
-    throw new functions.https.HttpsError(
-      'invalid-argument',
-      'You need to provide a ticker symbol'
+export const stockLivePrice = functions
+  .runWith({ memory: '1GB' })
+  .https.onCall(async (data) => {
+    const { ticker } = data;
+    if (typeof ticker !== 'string') {
+      throw new functions.https.HttpsError(
+        'invalid-argument',
+        'You need to provide a ticker symbol'
+      );
+    }
+
+    functions.logger.log('ticker', { ticker });
+
+    const response = await fetch(
+      `https://eodhistoricaldata.com/api/real-time/${ticker}?fmt=json&api_token=${getToken()}`
     );
-  }
+    const result = await response.json();
 
-  functions.logger.log('ticker', { ticker });
-
-  const response = await fetch(
-    `https://eodhistoricaldata.com/api/real-time/${ticker}?fmt=json&api_token=${getToken()}`
-  );
-  const result = await response.json();
-
-  return result;
-});
+    return result;
+  });
 
 /* =====
 Historical data
@@ -67,45 +71,47 @@ Historical data
 
 const stockHistoryCache = new NodeCache();
 
-export const stockHistory = functions.https.onCall(async (data) => {
-  const { ticker, from, to } = data;
-  if (typeof ticker !== 'string') {
-    throw new functions.https.HttpsError(
-      'invalid-argument',
-      'You need to provide a ticker symbol'
+export const stockHistory = functions
+  .runWith({ memory: '1GB' })
+  .https.onCall(async (data) => {
+    const { ticker, from, to } = data;
+    if (typeof ticker !== 'string') {
+      throw new functions.https.HttpsError(
+        'invalid-argument',
+        'You need to provide a ticker symbol'
+      );
+    }
+    if (typeof from !== 'string') {
+      throw new functions.https.HttpsError(
+        'invalid-argument',
+        'You need to provide a from date'
+      );
+    }
+    if (typeof to !== 'string') {
+      throw new functions.https.HttpsError(
+        'invalid-argument',
+        'You need to provide a to date'
+      );
+    }
+
+    functions.logger.log('ticker', { ticker });
+
+    const cacheKey = `${ticker}-${from}-${to}`;
+    const cache = stockHistoryCache.get(cacheKey);
+    if (cache) {
+      functions.logger.log('cache hit', { key: cacheKey });
+      return cache;
+    }
+
+    functions.logger.log('cache miss', { key: cacheKey });
+    const response = await fetch(
+      `https://eodhistoricaldata.com/api/eod/${ticker}?from=${from}&to=${to}&fmt=json&api_token=${getToken()}`
     );
-  }
-  if (typeof from !== 'string') {
-    throw new functions.https.HttpsError(
-      'invalid-argument',
-      'You need to provide a from date'
-    );
-  }
-  if (typeof to !== 'string') {
-    throw new functions.https.HttpsError(
-      'invalid-argument',
-      'You need to provide a to date'
-    );
-  }
+    const result = await response.json();
 
-  functions.logger.log('ticker', { ticker });
-
-  const cacheKey = `${ticker}-${from}-${to}`;
-  const cache = stockHistoryCache.get(cacheKey);
-  if (cache) {
-    functions.logger.log('cache hit', { key: cacheKey });
-    return cache;
-  }
-
-  functions.logger.log('cache miss', { key: cacheKey });
-  const response = await fetch(
-    `https://eodhistoricaldata.com/api/eod/${ticker}?from=${from}&to=${to}&fmt=json&api_token=${getToken()}`
-  );
-  const result = await response.json();
-
-  stockHistoryCache.set(cacheKey, result);
-  return result;
-});
+    stockHistoryCache.set(cacheKey, result);
+    return result;
+  });
 
 /* =====
 Stock info
