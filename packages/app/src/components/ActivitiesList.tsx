b9c15c5bd49aa40a78755735ecf8197fae057cfb
Qingqi Shi <qingqishi@live.com>
2020-12-18 02:16:24 +0000

    feat(activities): add month titles

--- packages/app/src/components/ActivitiesList.tsx
+++ packages/app/src/components/ActivitiesList.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, Fragment } from 'react';
 import styled from 'styled-components/macro';
+import dayjs from 'dayjs';
 import { ActivityItem, Modal, Type } from '@tuja/components';
 import { Activity } from '@tuja/libs';
 import ActivityDepositForm from './ActivityDepositForm';
@@ -14,6 +15,31 @@ const UpdateActivityContainer = styled.div`
   width: 500px;
   max-width: 100%;
   position: relative;
+`;
+
+const MonthTitle = styled(Type)`
+  position: sticky;
+  background-color: ${({ theme }) => theme.colors.backgroundMain};
+  z-index: ${({ theme }) => theme.zIndex.fixed};
+  top: 4rem;
+  @media (${({ theme }) => theme.breakpoints.minLaptop}) {
+    top: 3.5rem;
+  }
+
+  padding: ${({ theme }) => `${theme.spacings.s} ${theme.spacings.xs}`};
+  margin: 0 -${({ theme }) => theme.spacings.xs};
+  @media (${({ theme }) => theme.breakpoints.minTablet}) {
+    padding: ${({ theme }) => `${theme.spacings.s} ${theme.spacings.s}`};
+    margin: 0 -${({ theme }) => theme.spacings.s};
+  }
+  @media (${({ theme }) => theme.breakpoints.minLaptop}) {
+    padding: ${({ theme }) => `${theme.spacings.s} ${theme.spacings.m}`};
+    margin: 0 -${({ theme }) => theme.spacings.m};
+  }
+  @media (${({ theme }) => theme.breakpoints.minDesktop}) {
+    padding: ${({ theme }) => `${theme.spacings.s} ${theme.spacings.l}`};
+    margin: 0 -${({ theme }) => theme.spacings.l};
+  }
 `;
 
 function ActivitiesList() {
@@ -55,19 +81,30 @@ function ActivitiesList() {
     logEvent('delete_activity', { type: updateActivity?.type });
   };
 
+  const monthTitles = new Set();
+
   return (
     <div>
-      {[...portfolio.activities].reverse().map((activity, i) => (
-        <ActivityItem
-          key={activity.id}
-          activity={activity}
-          currency={portfolio.currency}
-          onClick={() => {
-            setUpdateActivityId(activity.id);
-            setShowUpdateActivity(true);
-          }}
-        />
-      ))}
+      {[...portfolio.activities].reverse().map((activity) => {
+        const monthTitle = dayjs(activity.date).format('MMMM YYYY');
+        const shouldShowTitle = !monthTitles.has(monthTitle);
+        monthTitles.add(monthTitle);
+        return (
+          <Fragment key={activity.id}>
+            {shouldShowTitle && (
+              <MonthTitle scale="h6">{monthTitle}</MonthTitle>
+            )}
+            <ActivityItem
+              activity={activity}
+              currency={portfolio.currency}
+              onClick={() => {
+                setUpdateActivityId(activity.id);
+                setShowUpdateActivity(true);
+              }}
+            />
+          </Fragment>
+        );
+      })}
 
       <Modal
         onClose={() => {
