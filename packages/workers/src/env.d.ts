d122548cb131c676d35d6a86b4567d487c210c75
Qingqi Shi <qingqishi@live.com>
2021-01-08 23:33:53 +0000

    fix(workers): how environment variables are accessed (#539)

--- packages/workers/src/env.d.ts
+++ packages/workers/src/env.d.ts
@@ -1,10 +1,1 @@
-declare namespace Global {
-  export interface ProcessEnv {
-    EOD_API_KEY?: string;
-  }
-  export interface Process {
-    env: ProcessEnv;
-  }
-}
-
-declare const process: Global.Process;
+declare const EOD_API_KEY: string | undefined;
