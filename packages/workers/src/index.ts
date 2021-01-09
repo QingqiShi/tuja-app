81adf4ac4e36210c1ab41b0f48545ee9a334eb5c
Qingqi Shi <qingqishi@live.com>
2021-01-09 16:21:12 +0000

    feat(workers): update live price endpoint due to workers fetch limit

--- packages/workers/src/index.ts
+++ packages/workers/src/index.ts
@@ -1,11 +1,11 @@
 import { handleSearch } from './handlers/handleSearch';
-import { handleLivePrices } from './handlers/handleLivePrices';
+import { handleLivePrice } from './handlers/handleLivePrice';
 
 const handleRequest = async (request: Request) => {
   const url = new URL(request.url);
 
   if (url.pathname === '/search') return handleSearch(request);
-  if (url.pathname === '/livePrices') return handleLivePrices(request);
+  if (url.pathname === '/livePrice') return handleLivePrice(request);
 
   return new Response('Not Found', { status: 404 });
 };
