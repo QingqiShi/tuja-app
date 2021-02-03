d3aa756d40de81e6c9468d9aa67f1de815881db2
Qingqi Shi <qingqishi@live.com>
2021-02-03 23:51:21 +0000

    test: add tests for the SearchSuggest component (#646)

---
+++ packages/components/src/components/inputs/SearchSuggest.test.tsx
@@ -0,0 +1,52 @@
+import { fireEvent, act } from '@testing-library/react';
+import { render } from '../../testUtils';
+import SearchSuggest from './SearchSuggest';
+
+jest.useFakeTimers('modern');
+
+test('fire search callback', async () => {
+  const handleSearch = jest.fn(async () => {});
+  const { getByPlaceholderText } = render(
+    <SearchSuggest onSearch={handleSearch} suggestions={[]} />
+  );
+  fireEvent.change(getByPlaceholderText('Search'), {
+    target: { value: 'test' },
+  });
+  await act(async () => {
+    jest.runAllTimers();
+  });
+  expect(handleSearch).toHaveBeenCalledWith('test');
+});
+
+test('render suggestions', async () => {
+  const handleClick = jest.fn();
+  const { getByText } = render(
+    <SearchSuggest
+      onSearch={async () => {}}
+      onClick={handleClick}
+      suggestions={[<div>a</div>, <div>b</div>]}
+    />
+  );
+  expect(getByText('a')).toBeInTheDocument();
+  expect(getByText('b')).toBeInTheDocument();
+
+  fireEvent.click(getByText('b'));
+  expect(handleClick).toHaveBeenCalledWith(1);
+});
+
+test('debounce search calls', async () => {
+  const handleSearch = jest.fn(async () => {});
+  const { getByPlaceholderText } = render(
+    <SearchSuggest onSearch={handleSearch} suggestions={[]} />
+  );
+  fireEvent.change(getByPlaceholderText('Search'), {
+    target: { value: 'testA' },
+  });
+  fireEvent.change(getByPlaceholderText('Search'), {
+    target: { value: 'testB' },
+  });
+  await act(async () => {
+    jest.runAllTimers();
+  });
+  expect(handleSearch).toHaveBeenCalledWith('testB');
+});
