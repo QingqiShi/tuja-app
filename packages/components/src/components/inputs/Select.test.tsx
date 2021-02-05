fc29aa7f77facf32d5eb9de13f5d1e0c92134de0
Qingqi Shi <qingqishi@live.com>
2021-02-05 19:05:28 +0000

    test: add tests for the Select component (#653)

---
+++ packages/components/src/components/inputs/Select.test.tsx
@@ -0,0 +1,38 @@
+import { fireEvent } from '@testing-library/react';
+import { render } from '../../testUtils';
+import Select from './Select';
+
+test('render label', async () => {
+  const { getByLabelText } = render(<Select label="Test Label" options={[]} />);
+  expect(getByLabelText('Test Label')).toBeInTheDocument();
+});
+
+test('render helper text', async () => {
+  const { getByText } = render(
+    <Select helperText="Test helper text" options={[]} />
+  );
+  expect(getByText('Test helper text')).toBeInTheDocument();
+});
+
+test('use internal state', async () => {
+  const { getByRole } = render(
+    <Select
+      options={[
+        { label: 'Test', value: 'a' },
+        { label: 'Test', value: 'b' },
+      ]}
+    />
+  );
+  fireEvent.change(getByRole('combobox'), { target: { value: 'b' } });
+  expect(getByRole('combobox')).toHaveValue('b');
+});
+
+test('render compact', async () => {
+  const { getByRole } = render(<Select options={[]} compact />);
+  expect(getByRole('combobox')).toHaveStyle('padding: 1rem 2rem 1rem 1.2rem;');
+});
+
+test('render disabled', async () => {
+  const { getByRole } = render(<Select options={[]} disabled />);
+  expect(getByRole('combobox')).toHaveStyle('opacity: 0.5;');
+});
