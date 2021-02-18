22d45c03e843ac205280d7ee4825b92a2fb5d38d
Qingqi Shi <qingqishi@live.com>
2021-02-18 23:24:09 +0000

    feat: add Sentry error reporting

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -2,12 +2,21 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import firebase from 'firebase/app';
 import 'firebase/analytics';
 import 'firebase/performance';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({
+  dsn:
+    'https://de80755a62ab40938ee851877d57417e@o527329.ingest.sentry.io/5643505',
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 firebase.initializeApp({
   apiKey: 'AIzaSyBHHoqcowFL7iaC2LP6QrP-pQyxUCqB3QM',
@@ -29,11 +38,13 @@ if (window.location.hostname === 'localhost') {
 
 ReactDOM.render(
   <StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
+    <Sentry.ErrorBoundary fallback={'An error has occurred'}>
+      <HelmetProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </HelmetProvider>
+    </Sentry.ErrorBoundary>
   </StrictMode>,
   document.getElementById('root')
 );
