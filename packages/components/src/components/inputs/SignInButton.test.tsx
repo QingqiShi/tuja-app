15f307b2ab7554ef07fa2c10ae0b4314fb0e8c0e
Qingqi Shi <qingqishi@live.com>
2021-02-19 19:30:12 +0000

    feat: update text color on error (#712)

--- packages/components/src/components/inputs/SignInButton.test.tsx
+++ packages/components/src/components/inputs/SignInButton.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '../../testUtils';
 import SignInButton from './SignInButton';
 
-test('toggle buttons', () => {
+test('render', () => {
   const { getByText } = render(
     <SignInButton
       icon={<>logo</>}
@@ -20,4 +20,38 @@ test('toggle buttons', () => {
     'background-color: #FFFFFF;'
   );
   expect(getByText('logo')).toHaveStyle('background-color: #000000;');
+  expect(getByText('Test').closest('button')).toHaveStyle(
+    'color: rgb(33, 26, 29);'
+  );
+});
+
+test('render dark button', () => {
+  const { getByText } = render(
+    <SignInButton icon={<>logo</>} shortText="Short" bgColor="#000">
+      Test
+    </SignInButton>
+  );
+  expect(getByText('Test').closest('button')).toHaveStyle('color: #FFFFFF;');
+});
+
+test('render light button in dark mode', () => {
+  const { getByText } = render(
+    <SignInButton icon={<>logo</>} shortText="Short" bgColor="#FFFFFF">
+      Test
+    </SignInButton>,
+    { theme: 'dark' }
+  );
+  expect(getByText('Test').closest('button')).toHaveStyle(
+    'color: rgb(33, 26, 29);'
+  );
+});
+
+test('render dark button in dark mode', () => {
+  const { getByText } = render(
+    <SignInButton icon={<>logo</>} shortText="Short" bgColor="#000">
+      Test
+    </SignInButton>,
+    { theme: 'dark' }
+  );
+  expect(getByText('Test').closest('button')).toHaveStyle('color: #FFFFFF;');
 });
--- packages/components/src/testUtils.tsx
+++ packages/components/src/testUtils.tsx
@@ -2,15 +2,20 @@ import { render as rtlRender } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import { getTheme } from './theme';
 
-export const render = (ui: React.ReactElement) => {
+interface RenderOptions {
+  theme: 'light' | 'dark';
+}
+
+export const render = (ui: React.ReactElement, options?: RenderOptions) => {
+  const { theme } = options ?? {};
   const results = rtlRender(
-    <ThemeProvider theme={getTheme('light')}>{ui}</ThemeProvider>
+    <ThemeProvider theme={getTheme(theme ?? 'light')}>{ui}</ThemeProvider>
   );
   return {
     ...results,
     rerender: (ui: React.ReactElement) =>
       results.rerender(
-        <ThemeProvider theme={getTheme('light')}>{ui}</ThemeProvider>
+        <ThemeProvider theme={getTheme(theme ?? 'light')}>{ui}</ThemeProvider>
       ),
   };
 };
