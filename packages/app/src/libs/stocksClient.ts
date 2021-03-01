491799105ea2aa0cd50db94d96786d02f6a8d485
Qingqi Shi <qingqishi@live.com>
2021-03-01 21:55:52 +0000

    fix: handle AbortError to reduce sentry noise

--- packages/app/src/libs/stocksClient.ts
+++ packages/app/src/libs/stocksClient.ts
@@ -120,13 +120,19 @@ export function fetchStockSearch(query: string) {
   const signal = controller.signal;
   return {
     fetch: async () => {
-      const response = await fetch(
-        `${process.env.REACT_APP_WORKERS_URL}/search?query=${encodeURIComponent(
-          query
-        )}`,
-        { signal }
-      );
-      return (await response.json()) as StockInfo[];
+      try {
+        const response = await fetch(
+          `${
+            process.env.REACT_APP_WORKERS_URL
+          }/search?query=${encodeURIComponent(query)}`,
+          { signal }
+        );
+        return (await response.json()) as StockInfo[];
+      } catch (e) {
+        if (e.name !== 'AbortError') {
+          throw e;
+        }
+      }
     },
     cancel: () => controller.abort(),
   };
@@ -141,23 +147,28 @@ export function fetchStocksPrices(
   const signal = controller.signal;
   return {
     fetch: async () => {
-      const data: StockPrice[] = await Promise.all(
-        tickers.map(async (ticker) =>
-          fetch(
-            `${
-              process.env.REACT_APP_WORKERS_URL
-            }/priceAt?ticker=${encodeURIComponent(ticker)}&at=${dayjs(
-              date
-            ).format(DATE_FORMAT)}&currency=${currency}`,
-            { signal }
-          ).then((res) => res.json())
-        )
-      );
-
-      return data.reduce(
-        (obj, price) => ({ ...obj, [price.ticker]: price }),
-        {} as { [ticker: string]: StockPrice }
-      );
+      try {
+        const data: StockPrice[] = await Promise.all(
+          tickers.map(async (ticker) =>
+            fetch(
+              `${
+                process.env.REACT_APP_WORKERS_URL
+              }/priceAt?ticker=${encodeURIComponent(ticker)}&at=${dayjs(
+                date
+              ).format(DATE_FORMAT)}&currency=${currency}`,
+              { signal }
+            ).then((res) => res.json())
+          )
+        );
+        return data.reduce(
+          (obj, price) => ({ ...obj, [price.ticker]: price }),
+          {} as { [ticker: string]: StockPrice }
+        );
+      } catch (e) {
+        if (e.name !== 'AbortError') {
+          throw e;
+        }
+      }
     },
     cancel: () => controller.abort(),
   };
