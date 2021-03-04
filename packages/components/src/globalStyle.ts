5f025818c5b232b81e18822a5cf734fadedbb103
Qingqi Shi <qingqishi@live.com>
2021-03-04 21:19:44 +0000

    feat: start using css custom properties for theming

--- packages/components/src/globalStyle.ts
+++ packages/components/src/globalStyle.ts
@@ -1,23 +1,47 @@
 import { createGlobalStyle } from 'styled-components';
+import dark from './themes/dark';
+import light from './themes/light';
+import { v } from './theme';
 
 export const GlobalStyle = createGlobalStyle`
   body {
-    color: ${({ theme }) => theme.colors.textOnBackground};
-    background-color: ${({ theme }) => theme.colors.backgroundMain};
-    box-sizing: border-box;
-    font-family: 'Inter', sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
+    ${dark}
+
+    @media (prefers-color-scheme: light) {
+      ${light}
+    }
+
+    &[data-theme=light] {
+      ${light}
+    }
+
+    &[data-theme=dark] {
+      ${dark}
+    }
+
+    --font-family: 'Inter', sans-serif;
+    --min-tablet: min-width: 706px;
+    --min-laptop: min-width: 1025px;
+    --min-desktop: min-width: 1441px;
   }
 
-  body * {
-    box-sizing: inherit;
-    font-size: inherit;
+  body {
+    box-sizing: border-box;
+    color: ${v.textMain};
+    background-color: ${v.backgroundMain};
+    font-family: ${v.fontFamily};
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    * {
+      box-sizing: inherit;
+      font-size: inherit;
+    }
   }
 
   html, body {
     font-size: 16px;
-    @media (${({ theme }) => theme.breakpoints.minLaptop}) {
+    @media (${v.minLaptop}) {
       font-size: 18px;
     }
   }
---
+++ packages/components/src/themes/light.ts
@@ -0,0 +1,11 @@
+import { css } from 'styled-components';
+
+export default css`
+  --background-main: #ffffff;
+  --background-raised: #ffffff;
+  --background-overlay: #ffffff;
+  --text-main: #211a1d;
+  --text-secondary: #382e32;
+  --text-gain: #49c569;
+  --text-loss: #f74b3c;
+`;
