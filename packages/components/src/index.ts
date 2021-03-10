2b569381e611a8c28594d440cb1f0b4d84be111d
Qingqi Shi <qingqishi@live.com>
2021-03-10 01:14:57 +0000

    feat: export components

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -24,6 +24,8 @@ export { default as TopLinearLoader } from './components/feedback/TopLinearLoade
 export { default as LinearLoader } from './components/feedback/LinearLoader';
 export { default as Modal } from './components/feedback/Modal';
 export { default as TopBar } from './components/controls/TopBar';
+export { default as DateRangeTabs } from './components/controls/DateRangeTabs';
+export { default as ButtonTertiary } from './components/controls/ButtonTertiary';
 export { default as ResponsiveSplit } from './components/layout/ResponsiveSplit';
 export { default as EdgePadding } from './components/layout/EdgePadding';
 
