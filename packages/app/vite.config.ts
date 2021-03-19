a4345dd1c42e3df109eeb505922b7c671c9afcdb
Qingqi Shi <qingqishi@live.com>
2021-03-19 01:47:59 +0000

    build: fix web worker build config (#872)

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,10 +1,25 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import webWorkerLoader from 'rollup-plugin-web-worker-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [
+    reactRefresh(),
+    {
+      ...webWorkerLoader({
+        inline: false,
+        sourcemap: true,
+        outputFolder: 'assets',
+        preserveFileNames: true,
+        pattern: /(.+)\?worker$/,
+        skipPlugins: ['liveServer', 'serve', 'livereload', 'vite:worker'],
+      }),
+      enforce: 'pre',
+      apply: 'build',
+    },
+  ],
   esbuild: {
     jsxInject: "import React from 'react'",
   },
@@ -15,5 +30,8 @@ export default defineConfig({
       '@tuja/components': '@tuja/components/src/index.ts',
       '@tuja/libs': '@tuja/libs/src/index.ts',
     },
+  },
+  build: {
+    brotliSize: false,
   },
 });
