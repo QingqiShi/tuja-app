42dc45ac214ebb4fc1281687a2f0a69ab294942d
Qingqi Shi <qingqishi@live.com>
2021-04-27 22:54:34 +0100

    feat: add Benefits component

---
+++ packages/components/src/components/molecules/Benefits.tsx
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+import EdgePadding from '../layout/EdgePadding';
+import { v } from '../../theme';
+
+const Container = styled.div`
+  margin-top: ${v.spacerXL};
+  padding-top: ${v.spacerXL};
+  padding-bottom: ${v.spacerXL};
+  background-color: ${v.backgroundRaised};
+`;
+
+const Title = styled.h2`
+  font-weight: ${v.fontBold};
+  margin: 0;
+  font-size: 1.5rem;
+  overflow: hidden;
+  text-align: center;
+  display: block;
+  width: 100%;
+
+  @media (${v.minTablet}) {
+    font-size: 1.8rem;
+  }
+
+  @media (${v.minTablet}) {
+    font-size: 2rem;
+  }
+
+  @media (${v.minDesktop}) {
+    font-size: 2.1rem;
+  }
+`;
+
+const BenefitsList = styled.dl`
+  margin-top: ${v.spacerM};
+
+  @media (${v.minTablet}) {
+    display: grid;
+    grid-gap: ${v.spacerS};
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (${v.minLaptop}) {
+    margin-top: ${v.spacerL};
+    grid-gap: ${v.spacerM};
+  }
+  @media (${v.minDesktop}) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+`;
+
+const BenefitItem = styled.div`
+  margin-bottom: ${v.spacerS};
+`;
+
+const BenefitIcon = styled.div`
+  font-size: 3rem;
+`;
+
+const BenefitName = styled.dt`
+  color: ${v.textMain};
+  font-weight: ${v.fontSemiBold};
+  font-size: 1.1rem;
+  margin: 0 0 ${v.spacerXS};
+`;
+
+const BenefitDescription = styled.dd`
+  color: ${v.textSecondary};
+  margin: 0;
+`;
+
+interface BenefitsProps {
+  title?: string;
+  benefits?: { name: string; description: string; icon?: React.ReactNode }[];
+}
+
+function Benefits({ title, benefits }: BenefitsProps) {
+  return (
+    <Container>
+      <EdgePadding>
+        {title && <Title>{title}</Title>}
+        {benefits && (
+          <BenefitsList>
+            {benefits.map(({ name, description, icon }) => (
+              <BenefitItem key={name}>
+                {icon && <BenefitIcon>{icon}</BenefitIcon>}
+                <BenefitName>{name}</BenefitName>
+                <BenefitDescription>{description}</BenefitDescription>
+              </BenefitItem>
+            ))}
+          </BenefitsList>
+        )}
+      </EdgePadding>
+    </Container>
+  );
+}
+
+export default Benefits;
