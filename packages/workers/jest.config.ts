5170e821b6e9421b18f8b93c4dc3a940695f958b
Qingqi Shi <qingqishi@live.com>
2021-05-31 17:44:37 +0100

    test: fix test config

--- packages/workers/jest.config.ts
+++ packages/workers/jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   displayName: 'workers',
   resetMocks: true,
+  testEnvironment: 'jsdom',
 };
 
 export default config;
