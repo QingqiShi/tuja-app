159b097ddfc69d6ddb0d0dcfe9714e2d8ad402d7
Qingqi Shi <qingqishi@live.com>
2021-06-09 21:28:57 +0100

    test: add tests for app stock client

---
+++ packages/app/src/libs/stocksClient.test.ts
@@ -0,0 +1,246 @@
+import dayjs from 'dayjs';
+import { TimeSeries } from '@tuja/libs';
+import {
+  fetchStockHistories,
+  fetchStockInfos,
+  fetchStockLivePrices,
+  fetchStockLogo,
+  fetchStockSearch,
+  fetchStocksPrices,
+} from './stocksClient';
+
+describe('fetchStockInfos', () => {
+  test('make network call', async () => {
+    const result = await fetchStockInfos(['IUSA.LSE']);
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost/bulkInfos?tickers=IUSA.LSE'
+    );
+    expect(result).toHaveLength(1);
+    expect(result[0]).toMatchObject({ Ticker: 'IUSA.LSE' });
+  });
+
+  test('batch requests', async () => {
+    const result = await fetchStockInfos([
+      'IWDP.LSE',
+      'AAPL.US',
+      'IUSA.LSE',
+      'IUS3.XETRA',
+      'SGLN.LSE',
+      'HMCH.LSE',
+      'T.US',
+      'USSC.LSE',
+      'VWRL.LSE',
+    ]);
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost/bulkInfos?tickers=IWDP.LSE,AAPL.US,IUSA.LSE,IUS3.XETRA,SGLN.LSE,HMCH.LSE'
+    );
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost/bulkInfos?tickers=T.US,USSC.LSE,VWRL.LSE'
+    );
+    expect(result).toHaveLength(9);
+    expect(result[0]).toMatchObject({ Ticker: 'IWDP.LSE' });
+    expect(result[5]).toMatchObject({ Ticker: 'HMCH.LSE' });
+    expect(result[6]).toMatchObject({ Ticker: 'T.US' });
+    expect(result[8]).toMatchObject({ Ticker: 'VWRL.LSE' });
+  });
+});
+
+describe('fetchStockLivePrices', () => {
+  test('make network call', async () => {
+    const result = await fetchStockLivePrices(['IUSA.LSE']);
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost/bulkLivePrices?tickers=IUSA.LSE'
+    );
+    expect(result).toHaveLength(1);
+    expect(result[0]).toMatchObject({
+      code: 'IUSA.LSE',
+      timestamp: 1614702540,
+      gmtoffset: 0,
+      open: 2787.9331,
+      high: 2797.25,
+      low: 2772.71,
+      close: 2777.375,
+      volume: 287103,
+      previousClose: 2784.875,
+      change: -7.5,
+      change_p: -0.2693,
+    });
+  });
+
+  test('batch requests', async () => {
+    const result = await fetchStockLivePrices([
+      'IWDP.LSE',
+      'AAPL.US',
+      'IUSA.LSE',
+      'IUS3.XETRA',
+      'SGLN.LSE',
+      'HMCH.LSE',
+      'T.US',
+      'USSC.LSE',
+      'VWRL.LSE',
+    ]);
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost/bulkLivePrices?tickers=IWDP.LSE,AAPL.US,IUSA.LSE,IUS3.XETRA,SGLN.LSE,HMCH.LSE'
+    );
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost/bulkLivePrices?tickers=T.US,USSC.LSE,VWRL.LSE'
+    );
+    expect(result).toHaveLength(9);
+    expect(result[0]).toMatchObject({ code: 'IWDP.LSE' });
+    expect(result[5]).toMatchObject({ code: 'HMCH.LSE' });
+    expect(result[6]).toMatchObject({ code: 'T.US' });
+    expect(result[8]).toMatchObject({ code: 'VWRL.LSE' });
+  });
+});
+
+describe('fetchStockHistories', () => {
+  test('make network call', async () => {
+    const result = await fetchStockHistories([
+      {
+        ticker: 'IUSA.LSE',
+        from: dayjs('2021-02-08', 'YYYY-MM-DD').toDate(),
+        to: dayjs('2021-02-09', 'YYYY-MM-DD').toDate(),
+      },
+    ]);
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost/bulkEods?query=%5B%7B%22ticker%22%3A%22IUSA.LSE%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%5D'
+    );
+    expect(result).toHaveLength(1);
+    expect(result[0]).toMatchObject({
+      ticker: 'IUSA.LSE',
+      from: dayjs('2021-02-08', 'YYYY-MM-DD').toDate(),
+      to: dayjs('2021-02-09', 'YYYY-MM-DD').toDate(),
+      closeSeries: expect.any(TimeSeries),
+      adjustedSeries: expect.any(TimeSeries),
+    });
+  });
+
+  test('batch requests', async () => {
+    const date = (str: string) => dayjs(str, 'YYYY-MM-DD').toDate();
+    const result = await fetchStockHistories([
+      { ticker: 'AAPL.US', from: date('2021-02-08'), to: date('2021-02-09') },
+      { ticker: 'HMCH.LSE', from: date('2021-02-08'), to: date('2021-02-09') },
+      { ticker: 'IUSA.LSE', from: date('2021-02-08'), to: date('2021-02-09') },
+      { ticker: 'IWDP.LSE', from: date('2021-02-08'), to: date('2021-02-09') },
+      {
+        ticker: 'IUS3.XETRA',
+        from: date('2021-02-08'),
+        to: date('2021-02-09'),
+      },
+      { ticker: 'SGLN.LSE', from: date('2021-02-08'), to: date('2021-02-09') },
+      { ticker: 'DIS.US', from: date('2021-02-08'), to: date('2021-02-09') },
+      { ticker: 'FB.US', from: date('2021-02-08'), to: date('2021-02-09') },
+    ]);
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost/bulkEods?query=%5B%7B%22ticker%22%3A%22AAPL.US%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%2C%7B%22ticker%22%3A%22HMCH.LSE%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%2C%7B%22ticker%22%3A%22IUSA.LSE%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%2C%7B%22ticker%22%3A%22IWDP.LSE%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%2C%7B%22ticker%22%3A%22IUS3.XETRA%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%2C%7B%22ticker%22%3A%22SGLN.LSE%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%5D'
+    );
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost/bulkEods?query=%5B%7B%22ticker%22%3A%22DIS.US%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%2C%7B%22ticker%22%3A%22FB.US%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%5D'
+    );
+    expect(result).toHaveLength(8);
+    expect(result[0]).toMatchObject({ ticker: 'AAPL.US' });
+    expect(result[5]).toMatchObject({ ticker: 'SGLN.LSE' });
+    expect(result[6]).toMatchObject({ ticker: 'DIS.US' });
+    expect(result[7]).toMatchObject({ ticker: 'FB.US' });
+  });
+});
+
+describe('fetchStockSearch', () => {
+  test('make network call', async () => {
+    const { fetch } = fetchStockSearch('Apple Inc');
+    const result = await fetch();
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost/search?query=Apple%20Inc',
+      expect.anything()
+    );
+    expect(result).toHaveLength(1);
+    expect(result[0]).toMatchObject({
+      Code: 'AAPL',
+      Country: 'USA',
+      Currency: 'USD',
+      Exchange: 'US',
+      ISIN: 'US0378331005',
+      Name: 'Apple Inc',
+      Ticker: 'AAPL.US',
+      Type: 'Common Stock',
+      previousClose: 126.74,
+    });
+  });
+
+  test('cancel call', async () => {
+    const { cancel, fetch } = fetchStockSearch('Apple Inc');
+    cancel();
+    expect(fetch()).rejects.toMatchObject({ message: 'fake abort error' });
+  });
+});
+
+describe('fetchStocksPrices', () => {
+  test('make network call', async () => {
+    const { fetch } = fetchStocksPrices(
+      ['AAPL.US'],
+      dayjs('2021-02-08', 'YYYY-MM-DD').toDate(),
+      'GBP'
+    );
+    const result = await fetch();
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost/priceAt?ticker=AAPL.US&at=2021-02-08&currency=GBP',
+      expect.anything()
+    );
+    expect(result).toMatchObject({
+      'AAPL.US': {
+        price: 127.145,
+        priceInCurrency: 90.120376,
+        ticker: 'AAPL.US',
+      },
+    });
+  });
+
+  test('make multiple network call', async () => {
+    const { fetch } = fetchStocksPrices(
+      ['AAPL.US', 'IUSA.LSE'],
+      dayjs('2021-02-08', 'YYYY-MM-DD').toDate(),
+      'GBP'
+    );
+    const result = await fetch();
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost/priceAt?ticker=AAPL.US&at=2021-02-08&currency=GBP',
+      expect.anything()
+    );
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost/priceAt?ticker=IUSA.LSE&at=2021-02-08&currency=GBP',
+      expect.anything()
+    );
+    expect(result).toMatchObject({
+      'AAPL.US': {
+        price: 127.145,
+        priceInCurrency: 90.120376,
+        ticker: 'AAPL.US',
+      },
+      'IUSA.LSE': {
+        price: 29.91625,
+        priceInCurrency: 29.91625,
+        ticker: 'IUSA.LSE',
+      },
+    });
+  });
+
+  test('cancel call', async () => {
+    const { cancel, fetch } = fetchStocksPrices(
+      ['AAPL.US'],
+      dayjs('2021-02-08', 'YYYY-MM-DD').toDate(),
+      'GBP'
+    );
+    cancel();
+    expect(fetch()).rejects.toMatchObject({ message: 'fake abort error' });
+  });
+});
+
+describe('fetchStockLogo', () => {
+  test('make network call', async () => {
+    const result = await fetchStockLogo('AAPL.US', 'Apple');
+    expect(window.fetch).toHaveBeenCalledWith(
+      'http://localhost/stockLogo?ticker=AAPL.US&name=Apple&size=108'
+    );
+    expect(result).toBe('data:application/octet-stream;base64,');
+  });
+});
--- packages/app/src/setupTests.ts
+++ packages/app/src/setupTests.ts
@@ -35,6 +35,10 @@ import.meta.env.VITE_API_URL = TEST_API;
 window.fetch = fetch;
 
 async function mockFetch(url: string, config: any) {
+  if (config?.signal?.aborted) {
+    throw new Error('fake abort error');
+  }
+
   switch (url) {
     case `${TEST_API}/bulkInfos?tickers=IUSA.LSE`: {
       return {
@@ -53,6 +57,200 @@ async function mockFetch(url: string, config: any) {
             previousCloseDate: '2021-03-02',
             Ticker: 'IUSA.LSE',
           },
+        ],
+      };
+    }
+    case `${TEST_API}/bulkInfos?tickers=IWDP.LSE,AAPL.US,IUSA.LSE,IUS3.XETRA,SGLN.LSE,HMCH.LSE`: {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => [
+          {
+            Code: 'IWDP',
+            Exchange: 'LSE',
+            Name: 'iShares II Public Limited Company - iShares Developed Markets Property Yield UCITS ETF',
+            Type: 'ETF',
+            Country: 'UK',
+            Currency: 'GBX',
+            ISIN: 'IE00B1FZS350',
+            previousClose: 2061.25,
+            previousCloseDate: '2021-06-08',
+            Ticker: 'IWDP.LSE',
+          },
+          {
+            Code: 'AAPL',
+            Exchange: 'US',
+            Name: 'Apple Inc',
+            Type: 'Common Stock',
+            Country: 'USA',
+            Currency: 'USD',
+            ISIN: 'US0378331005',
+            previousClose: 126.74,
+            previousCloseDate: '2021-06-08',
+            Ticker: 'AAPL.US',
+          },
+          {
+            Code: 'IUSA',
+            Exchange: 'LSE',
+            Name: 'iShares Core S&P 500 UCITS ETF USD Dist',
+            Type: 'ETF',
+            Country: 'UK',
+            Currency: 'GBX',
+            ISIN: 'IE0031442068',
+            previousClose: 2972.125,
+            previousCloseDate: '2021-06-08',
+            Ticker: 'IUSA.LSE',
+          },
+          {
+            Code: 'IUS3',
+            Exchange: 'XETRA',
+            Name: 'iShares S&P SmallCap 600 UCITS ETF USD (Dist)',
+            Type: 'ETF',
+            Country: 'Germany',
+            Currency: 'EUR',
+            ISIN: 'IE00B2QWCY14',
+            previousClose: 76.8,
+            previousCloseDate: '2021-06-08',
+            Ticker: 'IUS3.XETRA',
+          },
+          {
+            Code: 'SGLN',
+            Exchange: 'LSE',
+            Name: 'iShares Physical Gold ETC',
+            Type: 'ETF',
+            Country: 'UK',
+            Currency: 'GBX',
+            ISIN: 'IE00B4ND3602',
+            previousClose: 2612,
+            previousCloseDate: '2021-06-08',
+            Ticker: 'SGLN.LSE',
+          },
+          {
+            Code: 'HMCH',
+            Exchange: 'LSE',
+            Name: 'HSBC ETFs Public Limited Company - HSBC MSCI China UCITS ETF',
+            Type: 'ETF',
+            Country: 'UK',
+            Currency: 'GBX',
+            ISIN: 'IE00B44T3H88',
+            previousClose: 769.138,
+            previousCloseDate: '2021-06-08',
+            Ticker: 'HMCH.LSE',
+          },
+        ],
+      };
+    }
+    case `${TEST_API}/bulkInfos?tickers=T.US,USSC.LSE,VWRL.LSE`: {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => [
+          {
+            Code: 'T',
+            Exchange: 'US',
+            Name: 'AT&T Inc',
+            Type: 'Common Stock',
+            Country: 'USA',
+            Currency: 'USD',
+            ISIN: 'US00206R1023',
+            previousClose: 28.98,
+            previousCloseDate: '2021-06-08',
+            Ticker: 'T.US',
+          },
+          {
+            Code: 'USSC',
+            Exchange: 'LSE',
+            Name: 'SPDR MSCI USA Small Cap Value Weighted UCITS ETF USD Acc',
+            Type: 'ETF',
+            Country: 'UK',
+            Currency: 'USD',
+            ISIN: 'IE00BSPLC413',
+            previousClose: 57.075,
+            previousCloseDate: '2021-06-08',
+            Ticker: 'USSC.LSE',
+          },
+          {
+            Code: 'VWRL',
+            Exchange: 'LSE',
+            Name: 'Vanguard FTSE All-World UCITS ETF',
+            Type: 'ETF',
+            Country: 'UK',
+            Currency: 'GBP',
+            ISIN: 'IE00B3RBWM25',
+            previousClose: 83.65,
+            previousCloseDate: '2021-06-08',
+            Ticker: 'VWRL.LSE',
+          },
+        ],
+      };
+    }
+    case `${TEST_API}/bulkLivePrices?tickers=IUSA.LSE`: {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => [
+          {
+            code: 'IUSA.LSE',
+            timestamp: 1614702540,
+            gmtoffset: 0,
+            open: 2787.9331,
+            high: 2797.25,
+            low: 2772.71,
+            close: 2777.375,
+            volume: 287103,
+            previousClose: 2784.875,
+            change: -7.5,
+            change_p: -0.2693,
+          },
+        ],
+      };
+    }
+    case `${TEST_API}/bulkLivePrices?tickers=IWDP.LSE,AAPL.US,IUSA.LSE,IUS3.XETRA,SGLN.LSE,HMCH.LSE`: {
+      const fakePrice = {
+        timestamp: 1614702540,
+        gmtoffset: 0,
+        open: 2787.9331,
+        high: 2797.25,
+        low: 2772.71,
+        close: 2777.375,
+        volume: 287103,
+        previousClose: 2784.875,
+        change: -7.5,
+        change_p: -0.2693,
+      };
+      return {
+        ok: true,
+        status: 200,
+        json: async () => [
+          { ...fakePrice, code: 'IWDP.LSE' },
+          { ...fakePrice, code: 'AAPL.US' },
+          { ...fakePrice, code: 'IUSA.LSE' },
+          { ...fakePrice, code: 'IUS3.XETRA' },
+          { ...fakePrice, code: 'SGLN.LSE' },
+          { ...fakePrice, code: 'HMCH.LSE' },
+        ],
+      };
+    }
+    case `${TEST_API}/bulkLivePrices?tickers=T.US,USSC.LSE,VWRL.LSE`: {
+      const fakePrice = {
+        timestamp: 1614702540,
+        gmtoffset: 0,
+        open: 2787.9331,
+        high: 2797.25,
+        low: 2772.71,
+        close: 2777.375,
+        volume: 287103,
+        previousClose: 2784.875,
+        change: -7.5,
+        change_p: -0.2693,
+      };
+      return {
+        ok: true,
+        status: 200,
+        json: async () => [
+          { ...fakePrice, code: 'T.US' },
+          { ...fakePrice, code: 'USSC.LSE' },
+          { ...fakePrice, code: 'VWRL.LSE' },
         ],
       };
     }
@@ -89,25 +287,150 @@ async function mockFetch(url: string, config: any) {
         ],
       };
     }
-    case `${TEST_API}/bulkLivePrices?tickers=IUSA.LSE`: {
+    case `${TEST_API}/bulkEods?query=%5B%7B%22ticker%22%3A%22AAPL.US%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%2C%7B%22ticker%22%3A%22HMCH.LSE%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%2C%7B%22ticker%22%3A%22IUSA.LSE%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%2C%7B%22ticker%22%3A%22IWDP.LSE%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%2C%7B%22ticker%22%3A%22IUS3.XETRA%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%2C%7B%22ticker%22%3A%22SGLN.LSE%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%5D`: {
+      const fakeHistory = [
+        {
+          date: '2021-02-08',
+          open: 2829,
+          high: 2840.5,
+          low: 2827.75,
+          close: 2827.75,
+          adjusted_close: 2827.75,
+          volume: 213126,
+        },
+        {
+          date: '2021-02-09',
+          open: 2831,
+          high: 2834.653,
+          low: 2822.5,
+          close: 2823.875,
+          adjusted_close: 2823.875,
+          volume: 75581,
+        },
+      ];
       return {
         ok: true,
         status: 200,
         json: async () => [
           {
-            code: 'IUSA.LSE',
-            timestamp: 1614702540,
-            gmtoffset: 0,
-            open: 2787.9331,
-            high: 2797.25,
-            low: 2772.71,
-            close: 2777.375,
-            volume: 287103,
-            previousClose: 2784.875,
-            change: -7.5,
-            change_p: -0.2693,
+            ticker: 'AAPL.US',
+            from: '2021-02-08',
+            to: '2021-02-09',
+            history: fakeHistory,
+          },
+          {
+            ticker: 'HMCH.LSE',
+            from: '2021-02-08',
+            to: '2021-02-09',
+            history: fakeHistory,
+          },
+          {
+            ticker: 'IUSA.LSE',
+            from: '2021-02-08',
+            to: '2021-02-09',
+            history: fakeHistory,
+          },
+          {
+            ticker: 'IWDP.LSE',
+            from: '2021-02-08',
+            to: '2021-02-09',
+            history: fakeHistory,
+          },
+          {
+            ticker: 'IUS3.XETRA',
+            from: '2021-02-08',
+            to: '2021-02-09',
+            history: fakeHistory,
+          },
+          {
+            ticker: 'SGLN.LSE',
+            from: '2021-02-08',
+            to: '2021-02-09',
+            history: fakeHistory,
           },
         ],
+      };
+    }
+    case `${TEST_API}/bulkEods?query=%5B%7B%22ticker%22%3A%22DIS.US%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%2C%7B%22ticker%22%3A%22FB.US%22%2C%22from%22%3A%222021-02-08%22%2C%22to%22%3A%222021-02-09%22%7D%5D`: {
+      const fakeHistory = [
+        {
+          date: '2021-02-08',
+          open: 2829,
+          high: 2840.5,
+          low: 2827.75,
+          close: 2827.75,
+          adjusted_close: 2827.75,
+          volume: 213126,
+        },
+        {
+          date: '2021-02-09',
+          open: 2831,
+          high: 2834.653,
+          low: 2822.5,
+          close: 2823.875,
+          adjusted_close: 2823.875,
+          volume: 75581,
+        },
+      ];
+      return {
+        ok: true,
+        status: 200,
+        json: async () => [
+          {
+            ticker: 'DIS.US',
+            from: '2021-02-08',
+            to: '2021-02-09',
+            history: fakeHistory,
+          },
+          {
+            ticker: 'FB.US',
+            from: '2021-02-08',
+            to: '2021-02-09',
+            history: fakeHistory,
+          },
+        ],
+      };
+    }
+    case `${TEST_API}/search?query=Apple%20Inc`: {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => [
+          {
+            Code: 'AAPL',
+            Country: 'USA',
+            Currency: 'USD',
+            Exchange: 'US',
+            ISIN: 'US0378331005',
+            Name: 'Apple Inc',
+            Ticker: 'AAPL.US',
+            Type: 'Common Stock',
+            previousClose: 126.74,
+            previousCloseDate: '2021-06-08',
+          },
+        ],
+      };
+    }
+    case `${TEST_API}/priceAt?ticker=AAPL.US&at=2021-02-08&currency=GBP`: {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => ({
+          price: 127.145,
+          priceInCurrency: 90.120376,
+          ticker: 'AAPL.US',
+        }),
+      };
+    }
+    case `${TEST_API}/priceAt?ticker=IUSA.LSE&at=2021-02-08&currency=GBP`: {
+      return {
+        ok: true,
+        status: 200,
+        json: async () => ({
+          price: 29.91625,
+          priceInCurrency: 29.91625,
+          ticker: 'IUSA.LSE',
+        }),
       };
     }
 
@@ -116,8 +439,7 @@ async function mockFetch(url: string, config: any) {
         return { ok: true, status: 200, blob: () => new Blob() };
       }
 
-      // console.warn(`Unhandled request: ${url}`);
-      return fetch(url, config);
+      throw new Error(`Unhandled request: ${url}`);
     }
   }
 }
