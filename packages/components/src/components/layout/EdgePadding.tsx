37f2abf752aed63c3a284565d7b5ddf940efc9ed
Qingqi Shi <qingqishi@live.com>
2021-06-13 20:37:45 +0100

    fix: side padding ResponsiveSplit

--- packages/components/src/components/layout/EdgePadding.tsx
+++ packages/components/src/components/layout/EdgePadding.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { v } from '../../theme';
 
 const EdgePadding = styled.div`
-  width: 100%;
   max-width: ${v.maxLayoutWidth};
   padding-left: calc(env(safe-area-inset-left) + ${v.leftRightPadding});
   padding-right: calc(env(safe-area-inset-right) + ${v.leftRightPadding});
