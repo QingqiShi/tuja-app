62e2b4f42ccf9f90106daf335787dbb9a90a8a7b
Qingqi Shi <qingqishi@live.com>
2021-06-19 02:20:20 +0100

    feat: increase sample points for annual returns

--- packages/app/src/analytics/AnnualReturns/annualReturns.worker.ts
+++ packages/app/src/analytics/AnnualReturns/annualReturns.worker.ts
@@ -14,72 +14,40 @@ async function handler({
   inflationRate,
 }: AnalyticsProps) {
   const tickers = assets.map((a) => a.ticker);
-  const dailyInflation = inflationRate / 365;
 
   const { stocksInfo, stocksHistory, dateRange } = await prefetchStocksHistory(
     tickers,
     baseCurrency
   );
 
-  const returns = new TimeSeries();
-
-  const prev = assets.reduce(
-    (obj, asset) => ({
-      ...obj,
-      [asset.ticker]: { percentage: asset.percentage },
-    }),
-    {} as {
-      [ticker: string]: {
-        prevPrice?: number;
-        prevValue?: number;
-        percentage: number;
-      };
-    }
-  );
-
+  const annualReturns = [];
   for (
-    let i = dayjs(dateRange.startDate);
+    let i = dayjs(dateRange.startDate).add(1, 'year');
     i.isBefore(dateRange.endDate);
     i = i.add(1, 'day')
   ) {
-    const date = i.toDate();
+    const currentDate = i.toDate();
+    const lastYearDate = i.subtract(1, 'year').toDate();
 
-    const newValue = assets.reduce((sum, { ticker }) => {
+    const newValue = assets.reduce((sum, { ticker, percentage }) => {
       const currentPrice = exchangeCurrency(
-        stocksHistory[ticker].adjusted.get(date),
+        stocksHistory[ticker].adjusted.get(currentDate),
         stocksInfo[ticker].Currency ?? baseCurrency,
         baseCurrency,
-        (forexPair) => stocksHistory[forexPair]?.close.get(date)
+        (forexPair) => stocksHistory[forexPair]?.close.get(currentDate)
       );
-      const prevPrice = prev[ticker].prevPrice ?? currentPrice;
-      const prevAssetValue = prev[ticker].prevValue ?? prev[ticker].percentage;
-      const prevAssetValueInflationAdjusted =
-        prevAssetValue * (1 - dailyInflation);
+      const prevPrice = exchangeCurrency(
+        stocksHistory[ticker].adjusted.get(lastYearDate),
+        stocksInfo[ticker].Currency ?? baseCurrency,
+        baseCurrency,
+        (forexPair) => stocksHistory[forexPair]?.close.get(lastYearDate)
+      );
 
-      const assetValue =
-        prevAssetValueInflationAdjusted +
-        prevAssetValueInflationAdjusted *
-          ((currentPrice - prevPrice) / prevPrice);
-
-      prev[ticker].prevValue = assetValue;
-      prev[ticker].prevPrice = currentPrice;
-      return sum + assetValue;
+      const returns = (currentPrice - prevPrice) / prevPrice - inflationRate;
+      return sum + returns * percentage;
     }, 0);
 
-    returns.data.push([date, newValue]);
-  }
-
-  const annualReturns = [];
-  const start = dayjs(dateRange.startDate).add(1, 'year').startOf('year');
-  const end = dayjs(dateRange.endDate).startOf('year');
-  for (
-    let i = start.add(1, 'year');
-    !i.isAfter(end, 'year');
-    i = i.add(1, 'year')
-  ) {
-    const prev = returns.get(i.subtract(1, 'year').toDate());
-    const current = returns.get(i.toDate());
-    annualReturns.push((current - prev) / prev);
+    annualReturns.push(newValue);
   }
 
   return annualReturns;
